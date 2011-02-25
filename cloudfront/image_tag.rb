bf5e31f48b07afc951052b33af81d647f5b472c8
locomundo <fpaganelli@gmail.com>
2011-02-25 02:23:16 -0800

    Edited cloudfront/image_tag.rb via GitHub

--- cloudfront/image_tag.rb
+++ cloudfront/image_tag.rb
@@ -1,18 +1,18 @@
 module Kulitzer
   module CloudFrontImageTag
 
-    S3_MEDIA_PREFIX = 'http://s3.amazonaws.com/media.kulitzer.com'
-    CF_MEDIA_PREFIX = 'http://media%d.kulitzer.com'
+    S3_PREFIX = 'http://s3.amazonaws.com/media.kulitzer.com'
+    CF_PREFIX = 'http://media%d.kulitzer.com'
 
     def image_tag(source, options = {})
       # For production, re-write url to use CloudFront
-      if Rails.env.include?('production') && source.starts_with?(S3_MEDIA_PREFIX)
+      if Rails.env.include?('production') && source.starts_with?(S3_PREFIX)
         # re-write to use a cloudfront url eg
-        # http://s3.amazonaws.com/media.kulitzer.com/content/ContestCovers/FOO.jpg becomes
-        # http://mediaZ.kulitzer.com/content/ContestCovers/FOO.jpg where Z = 0..3
+        # http://s3.amazonaws.com/media.kulitzer.com/foo.jpg becomes
+        # http://mediaZ.kulitzer.com/foo.jpg where Z = 0..3
 
-        cf_host = CF_MEDIA_PREFIX % rand(4)
-        source = source.sub(S3_MEDIA_PREFIX, cf_host)
+        cf_host = CF_PREFIX % rand(4)
+        source = source.sub(S3_PREFIX, cf_host)
       end
 
       super
